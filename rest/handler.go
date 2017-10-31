d37da247f84d5736c80ba73ed7de0ee71de46f91
Linzy Hu <linzy.hu@gmail.com>
2017-10-31 12:11:37 +0800

    add rest interface to sdk

---
+++ rest/handler.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"net/http"
+
+	sdk "github.com/bitmark-inc/bitmark-sdk-go"
+	"github.com/gin-gonic/gin"
+)
+
+func handleCreateAccount() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		acct, _ := sdk.NewAccount(cfg.Network)
+
+		c.JSON(http.StatusOK, gin.H{
+			"account_number":  acct.AccountNumber(),
+			"seed":            acct.Seed(),
+			"recovery_phrase": acct.RecoveryPhrase(),
+		})
+	}
+}
+
+type issueRequest struct {
+	Seed          string            `json:"seed"`
+	FilePath      string            `json:"file_path"`
+	Accessibility sdk.Accessibility `json:"accessibility"`
+	Name          string            `json:"property_name"`
+	Metadata      map[string]string `json:"property_metadata"`
+	Quantity      int               `json:"quantity"`
+}
+
+func handleIssue() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req issueRequest
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request body"})
+			return
+		}
+
+		acct, err := sdk.AccountFromSeed(req.Seed)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "invalid seed"})
+			return
+		}
+
+		assetId, bitmarkIds, err := acct.IssueNewBitmarks(req.FilePath, req.Accessibility, req.Name, req.Metadata, req.Quantity)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "invalid seed"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"asset_id":    assetId,
+			"bitmark_ids": bitmarkIds,
+		})
+	}
+}
+
+type transferRequest struct {
+	Seed      string `json:"seed"`
+	BitmarkId string `json:"bitmark_id"`
+	Receiver  string `json:"receiver"`
+}
+
+func handleTransfer() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req transferRequest
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request body"})
+			return
+		}
+
+		acct, err := sdk.AccountFromSeed(req.Seed)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "invalid seed"})
+			return
+		}
+
+		txId, err := acct.TransferBitmark(req.BitmarkId, req.Receiver)
+		if err != nil {
+			log.Error(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"message": err})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"txId": txId,
+		})
+	}
+}
+
+type downloadRequest struct {
+	Seed      string `json:"seed"`
+	BitmarkId string `json:"bitmark_id"`
+}
+
+func handleDownloadAsset() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req transferRequest
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request body"})
+			return
+		}
+
+		acct, err := sdk.AccountFromSeed(req.Seed)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "invalid seed"})
+			return
+		}
+
+		content, err := acct.DownloadAsset(req.BitmarkId)
+		if err != nil {
+			log.Error(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"message": "download failed"})
+			return
+		}
+
+		c.Data(http.StatusOK, http.DetectContentType(content), content)
+		return
+	}
+}
---
+++ rest/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+
+	sdk "github.com/bitmark-inc/bitmark-sdk-go"
+	"github.com/bitmark-inc/logger"
+	"github.com/gin-gonic/gin"
+	"github.com/hashicorp/hcl"
+)
+
+var (
+	cfg *config
+	log *logger.L
+)
+
+type config struct {
+	Network sdk.Network `hcl:"network"`
+	Port    int         `hcl:"port"`
+	DataDir string      `hcl:"datadir"`
+}
+
+func init() {
+	var confpath string
+	flag.StringVar(&confpath, "conf", "", "Specify configuration file")
+	flag.Parse()
+
+	cfg = readConfig(confpath)
+
+	if err := logger.Initialise(logger.Configuration{
+		Directory: cfg.DataDir,
+		File:      "trade.log",
+		Size:      1048576,
+		Count:     10,
+		Levels:    map[string]string{"DEFAULT": "info"},
+	}); err != nil {
+		panic(fmt.Sprintf("logger initialization failed: %s", err))
+	}
+
+	log = logger.New("")
+}
+
+func readConfig(confpath string) *config {
+	var cfg config
+
+	dat, err := ioutil.ReadFile(confpath)
+	if err != nil {
+		panic(fmt.Sprintf("unable to read the configuration: %v", err))
+	}
+
+	if err = hcl.Unmarshal(dat, &cfg); nil != err {
+		panic(fmt.Sprintf("unable to parse the configuration: %v", err))
+	}
+
+	return &cfg
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/account", handleCreateAccount())
+	r.POST("/issue", handleIssue())
+	r.POST("/transfer", handleTransfer())
+	r.POST("/download", handleDownloadAsset())
+	r.Run(fmt.Sprintf(":%d", cfg.Port))
+}
